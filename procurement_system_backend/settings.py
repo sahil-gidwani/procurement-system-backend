965403d8ff8f602faee23fa1c880ac1c73ddd8d3
sahil-gidwani <sahilgdwn@gmail.com>
2023-12-16 01:40:39 +0530

    Update settings.py with environment variables

--- procurement_system_backend/settings.py
+++ procurement_system_backend/settings.py
@@ -1,8 +1,5 @@
 from pathlib import Path
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,13 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.environ.get("DEBUG")
 
-# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -73,8 +69,8 @@ WSGI_APPLICATION = 'procurement_system_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv("DB_ENGINE"),
-        'NAME': BASE_DIR / os.getenv("DB_NAME"),
+        'ENGINE': os.environ.get("DB_ENGINE"),
+        'NAME': BASE_DIR / os.environ.get("DB_NAME"),
     }
 }
 
