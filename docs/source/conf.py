2d7407f81deb01466231540d049a79774ed6dc3a
GitHub Action <action@github.com>
2023-12-16 08:51:15 +0000

    Auto-format code with Black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,26 +8,26 @@ import sys
 import django
 
 # Assuming conf.py is located in docs/source/
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 sys.path.insert(0, parent_dir)
 
 # Set up Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'procurement_system_backend.settings'
+os.environ["DJANGO_SETTINGS_MODULE"] = "procurement_system_backend.settings"
 django.setup()
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Smart Procurement System'
-copyright = '2024, Sahil Gidwani'
-author = 'Sahil Gidwani'
-release = '0.1'
+project = "Smart Procurement System"
+copyright = "2024, Sahil Gidwani"
+author = "Sahil Gidwani"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',      # Automatically document Python code
+    "sphinx.ext.autodoc",  # Automatically document Python code
     # 'sphinx.ext.autosummary'   # Generate autodoc summaries
     # 'sphinx.ext.napoleon',     # Google-style or NumPy-style docstrings
     # 'sphinx.ext.todo',         # Todo directive for marking incomplete sections
@@ -38,13 +38,12 @@ extensions = [
     # 'sphinx.ext.imgmath',      # Include math equations using LaTeX
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
