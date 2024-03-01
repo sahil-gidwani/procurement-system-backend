f25a931d3dcc5bdb4c242fb18e37616fd87e185f
Sahil Gidwani <sahilgdwn@gmail.com>
2024-03-01 23:02:38 +0530

    Update celery flower log level

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ python manage.py migrate
 # TODO: Try using pool prefork instead of solo for production
 celery --app procurement_system_backend worker --loglevel=INFO --task-events --pool=solo &
 celery --app procurement_system_backend beat --loglevel=INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-# celery --app procurement_system_backend flower
+# celery --app procurement_system_backend flower --loglevel=INFO &
