e1bba3dae705d4004e85ec126f290d48cb7ad601
Sahil Gidwani <sahilgdwn@gmail.com>
2024-03-01 12:50:34 +0530

    Add blank line in tasks.py file

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from django.core.mail import send_mail
 from django.conf import settings
 
+
 @shared_task
 def send_password_reset_email(user_email, password_reset_url):
     send_mail(
